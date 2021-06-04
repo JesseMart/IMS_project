825e42af59444ab6bbef78130eff3b844ea457ed
Jesse Martinez <jessemartinez452@gmail.com>
2021-06-04 05:40:03 -0500

    ADDED README file

--- src/main/java/com/qa/utils/DBConfig.java
+++ src/main/java/com/qa/utils/DBConfig.java
@@ -9,7 +9,7 @@ public class DBConfig {
 	
 	public static String URL = "jdbc:mysql://localhost:3306/ism_tables";
 	public static String USERNAME = "root";
-	public static String PASSW = "mJ051396";
+	public static String PASSW = "";
 	
 	
 
